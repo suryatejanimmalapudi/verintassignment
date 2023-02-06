f6ab68fa18e6a103c24330e1368c6ee4c7ffe3d2
surya <suryanimmalapudi@gmail.com>
2023-02-07 00:21:41 +0530

    back end api

---
+++ src/main/java/com/luv2code/ecommerce/entity/Country.java
@@ -0,0 +1,47 @@
+package com.luv2code.ecommerce.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Getter;
+import lombok.Setter;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+import java.util.List;
+
+@Entity
+@Table(name="country")
+@Getter
+@Setter
+public class Country {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private int id;
+
+    @Column(name="code")
+    private String code;
+
+    @Column(name="name")
+    private String name;
+
+    @OneToMany(mappedBy = "country")
+    @JsonIgnore
+    private List<State> states;
+
+}
+
+
+
+
+
+
+
+
+
+
