e42ca2c46fbedd559d8693f38de82ac8d7e1f73f
surya <suryanimmalapudi@gmail.com>
2023-02-07 11:51:58 +0530

    added docker

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,31 +3,31 @@ INSERT INTO PRODUCT_CATEGORY(CATEGORY_NAME) VALUES ('BOOKS');
 INSERT INTO PRODUCT (SKU, NAME, DESCRIPTION, IMAGE_URL, ACTIVE, UNITS_IN_STOCK,
 UNIT_PRICE, CATEGORY_ID,DATE_CREATED,discount,finalprice)
 VALUES ('BOOK-TECH-1000', 'JavaScript - The Fun Parts', 'Learn JavaScript',
-'assets/images/products/placeholder.png'
+'assets/images/products/books/book-luv2code-1000.png'
 ,1,100,1000,1, NOW(),20,800);
 
 INSERT INTO PRODUCT (SKU, NAME, DESCRIPTION, IMAGE_URL, ACTIVE, UNITS_IN_STOCK,
 UNIT_PRICE, CATEGORY_ID,DATE_CREATED,discount,finalprice)
 VALUES ('BOOK-TECH-1001', 'Spring Framework Tutorial', 'Learn Spring',
-'assets/images/products/placeholder.png'
+'assets/images/products/books/book-luv2code-1000.png'
 ,1,100,1000,1, NOW(),20,800);
 
 INSERT INTO PRODUCT (SKU, NAME, DESCRIPTION, IMAGE_URL, ACTIVE, UNITS_IN_STOCK,
 UNIT_PRICE, CATEGORY_ID,DATE_CREATED,discount,finalprice)
 VALUES ('BOOK-TECH-1002', 'Kubernetes - Deploying Containers', 'Learn Kubernetes',
-'assets/images/products/placeholder.png'
+'assets/images/products/books/book-luv2code-1000.png'
 ,1,100,1000,1, NOW(),20,800);
 
 INSERT INTO PRODUCT (SKU, NAME, DESCRIPTION, IMAGE_URL, ACTIVE, UNITS_IN_STOCK,
 UNIT_PRICE, CATEGORY_ID,DATE_CREATED,discount,finalprice)
 VALUES ('BOOK-TECH-1003', 'Internet of Things (IoT) - Getting Started', 'Learn IoT',
-'assets/images/products/placeholder.png'
+'assets/images/products/books/book-luv2code-1000.png'
 ,1,100,1000,1, NOW(),20,800);
 
 INSERT INTO PRODUCT (SKU, NAME, DESCRIPTION, IMAGE_URL, ACTIVE, UNITS_IN_STOCK,
 UNIT_PRICE, CATEGORY_ID,DATE_CREATED,discount,finalprice)
 VALUES ('BOOK-TECH-1004', 'The Go Programming Language: A to Z', 'Learn Go',
-'assets/images/products/placeholder.png'
+'assets/images/products/books/book-luv2code-1000.png'
 ,1,100,1000,1, NOW(),20,800);
 
 
